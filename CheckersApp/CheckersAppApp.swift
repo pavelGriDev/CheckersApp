4d160faf277132aa63a985631ac74d84e71480d7
Pavel Gritskov <pavel_ios_dev@mobilexapps.com>
2025-06-11 02:02:26 +0400

    Initial Commit

---
+++ CheckersApp/CheckersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckersAppApp.swift
+//  CheckersApp
+//
+//  Created by Pavel Gritskov on 11.06.25.
+//
+
+import SwiftUI
+
+@main
+struct CheckersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckersApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CheckersApp
+//
+//  Created by Pavel Gritskov on 11.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
